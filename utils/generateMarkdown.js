0ffc68ab5cbd9fb5291c4159d5f31d19d8d95b03
Jordan Roberts <jdrobs19@gmail.com>
2020-09-26 14:33:18 -0600

    set up README format

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,39 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Table of Contents
+  [Description](### Description)
+  [Installation](### Installation)
+  ### [Usage](#Usage)
+  ### [Contributing](#Contributing)
+  ### [Tests](#Tests)
+  ### [License](#License)
+  ### [Questions](#Questions)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution Guidelines
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## License
+  ${data.license}
+
+  ## Questions
+  [GitHub](${data.github})
+  ### ${data.email}
+  ### ${data.questions}
 
 `;
 }
